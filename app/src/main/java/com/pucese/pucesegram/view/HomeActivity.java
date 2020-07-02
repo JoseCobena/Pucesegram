7b01f7620eed35c9b43f624485fb27e444294b1d
JoseCobena <jose.cobena@pucese.edu.ec>
2020-07-02 11:16:43 -0500

    Login & Sign Out funcionando

--- app/src/main/java/com/pucese/pucesegram/view/HomeActivity.java
+++ app/src/main/java/com/pucese/pucesegram/view/HomeActivity.java
@@ -3,6 +3,7 @@ package com.pucese.pucesegram.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,12 +14,11 @@ import com.pucese.pucesegram.R;
 public class HomeActivity extends AppCompatActivity {
     private Button mButtonSignOut;
     private FirebaseAuth mAuth;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cardview_picture);
+        setContentView(R.layout.activity_home);
+        showToolbar(getResources().getString(R.string.toolbar_home),false);
         mAuth = FirebaseAuth.getInstance();
         mButtonSignOut = (Button) findViewById(R.id.signOut);
         mButtonSignOut.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +29,12 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
-
+    }
+    public void showToolbar(String tittle, boolean upButton)
+    {
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
